7ea51026f8561bca9ee9f8aa525fc243257c392b
ruanleao <ruanleao.ib@gmai.com>
2025-09-17 19:44:42 -0300

    first commit

---
+++ Js01.js
@@ -0,0 +1,5 @@
+// Mostra uma janela de alerta
+alert("Meu primeiro código JavaScript está muito legal!");
+
+//Escreve uma mensagem no console do navegador(abra F12 > Console para ver)
+console.log("Mensagem no console: JavaScript carregado com sucesso.")
